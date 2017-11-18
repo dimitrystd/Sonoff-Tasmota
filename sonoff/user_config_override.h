44e0cc753f53cd989b0041a8d6b53712316705c9
Dmitriy Studinskiy <dms@smartling.com>
2017-11-18 13:43:57 -0500

    Upgraded configuration

--- sonoff/user_config_override.h
+++ sonoff/user_config_override.h
@@ -51,10 +51,10 @@
 #endif
 #define WIFI_CONFIG_TOOL       WIFI_RETRY
 
-#ifdef PROJECT
-    #undef PROJECT
-#endif
+#undef PROJECT
 #define PROJECT                "ed_bedroom" // Electrodragon http://www.electrodragon.com/product/wifi-iot-relay-board-based-esp8266/
+#undef MODULE
+#define MODULE                 ELECTRODRAGON
 
 #ifdef SERIAL_LOG_LEVEL
     #undef SERIAL_LOG_LEVEL
@@ -80,10 +80,17 @@
     #define MQTT_CLIENT_ID       PROJECT "_%06X"       // [MqttClient] Also fall back topic using Chip Id = last 6 characters of MAC address
 #endif
 
+#undef TELE_PERIOD
+#define TELE_PERIOD          120               // [TelePeriod] Telemetry (0 = disable, 10 - 3600 seconds)
+
 #ifdef USE_DOMOTICZ
     #undef USE_DOMOTICZ
 #endif
 
+#ifdef FRIENDLY_NAME
+    #undef FRIENDLY_NAME
+    #define FRIENDLY_NAME        "smart-home-us - " PROJECT // [FriendlyName] Friendlyname up to 32 characters used by webpages and Alexa
+#endif
 #ifdef USE_DISCOVERY
     #undef USE_DISCOVERY
 #endif
@@ -98,10 +105,12 @@
 #undef TIME_DST
 #undef TIME_STD
 #undef APP_TIMEZONE
+#undef APP_LEDSTATE
 // second Sunday in March and ends on the first Sunday in November, with the time changes taking place at 2:00 a.m. local time
 #define TIME_DST               Second, Sun, Mar, 2, +120  // Last sunday in march at 02:00 +120 minutes
 #define TIME_STD               First, Sun, Nov, 2, +60   // Last sunday in october 02:00 +60 minutes
 #define APP_TIMEZONE           -5                 // [Timezone] +1 hour (Amsterdam) (-12 .. 12 = hours from UTC, 99 = use TIME_DST/TIME_STD)
+#define APP_LEDSTATE           LED_POWER_MQTT
 
 #ifdef WS2812_LEDS
     #undef WS2812_LEDS
